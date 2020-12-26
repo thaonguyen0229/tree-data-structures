d30094e2df3088832c4a88137360ede9f1b627e6
thaonguyen0229 <47158178+thaonguyen0229@users.noreply.github.com>
2020-12-25 19:37:12 -0600

    Upload testing.py

---
+++ testing.py
@@ -0,0 +1,34 @@
+from AVL import AVL
+from BTree import BTree
+treeType = input("Enter AVL or BTree: ")
+
+while treeType != "AVL" and treeType != "BTree":
+    treeType = input("Enter AVL or BTree: ")
+
+value = input("Enter an integer value: ")
+
+while type(value) is not int:
+    try:
+        value = int(value)
+    except:
+        print(value, "is not an integer")
+        value = input("Enter an integer value: ")
+
+if treeType == "AVL":
+    tree = AVL(value)
+else:
+    tree = BTree(value)
+
+print(tree)
+new = input("Enter an integer or X to quit: ")
+while new != "X":
+    try:
+        new = int(new)
+    except:
+        print(new, "is not an integer")
+        new = input("Enter an integer value: ")
+        continue
+    tree.insert(new)
+    print(tree)
+    new = input("Enter an integer or X to quit: ")
+
